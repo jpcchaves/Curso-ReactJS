3617c5f2e3a1d186dd451ea2ef569880b851f312
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-05-28 12:53:00 -0300

    exemplos de onclick

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -1,17 +1,29 @@
 const Events = () => {
-    const handleMyEvent = (e) => {
-        console.log(e)
+  const handleMyEvent = (e) => {
+    console.log(e);
 
-        console.log("Ativou o evento")
-    }
-    
-    return (
-        <div className="component">
-            <div>
-                <button onClick={handleMyEvent}>Clique aqui </button>
-            </div>
-        </div>
-    )
-}
+    console.log("Ativou o evento");
+  };
 
-export default Events
+  return (
+    <div className="component">
+      <div>
+        <button onClick={handleMyEvent}>Clique aqui </button>
+      </div>
+      <div>
+        <button onClick={() => console.log("Clicou")}>
+          Clique aqui também
+        </button>
+      </div>
+      <div>
+          <button onClick={() => {
+              if(true){
+                  console.log(`Isso não deveria existir kk`)
+              }
+          }}>Duvido você clicar aqui</button>
+      </div>
+    </div>
+  );
+};
+
+export default Events;

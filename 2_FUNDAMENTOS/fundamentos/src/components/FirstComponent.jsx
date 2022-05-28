f76c1b98951581100735c99d423f374438faedec
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-05-28 12:16:04 -0300

    hierarquia de componentes

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
@@ -1,5 +1,6 @@
 // STYLES 
 import './FirstComponent.css'
+import MyComponent from './MyComponent';
 
 const FirstComponent = () => {
     // Componente funcional
@@ -7,8 +8,13 @@ const FirstComponent = () => {
         
         <div className='component'>
             {/* Comentário no JSX  */}
-            <h1>Meu primeiro componente</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure earum doloremque tenetur soluta perferendis odio, quaerat ipsum placeat distinctio delectus facere impedit expedita suscipit rem? Officiis sapiente recusandae illo beatae!</p>
+            <h1>
+                Meu primeiro componente
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure earum doloremque tenetur soluta perferendis odio, quaerat ipsum placeat distinctio delectus facere impedit expedita suscipit rem? Officiis sapiente recusandae illo beatae!
+            </p>
+            <MyComponent/>
         </div>
     )
 }
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+const MyComponent = () => {
+
+    return (
+        <div className="component">
+            {/* Algum Comentário */}
+            <h2>Eu estou sendo reaproveitado em vários lugares</h2>
+            <h3>Aula sobre a hierarquia de componentes  </h3>
+        </div>
+    )
+
+}
+
+export default MyComponent

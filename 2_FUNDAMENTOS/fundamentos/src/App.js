788464f353351294743a279807700d890a5d1707
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-05-28 12:47:57 -0300

    eventos onclick

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 
 // Components
 import FirstComponent from './components/FirstComponent';
+import MyComponent from './components/MyComponent';
 import TemplateExpressions from './components/TemplateExpressions';
+import Events from './components/Events';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <h1>Fundamentos React</h1>
       <FirstComponent/>
       <TemplateExpressions/>
+      <MyComponent/>
+      <Events/>
     </div>
   );
 }

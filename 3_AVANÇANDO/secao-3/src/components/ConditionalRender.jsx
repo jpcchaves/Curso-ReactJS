ebb9c0bc146c12832b9da63d0f3a713c998a43cb
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-05-29 13:05:18 -0300

    verificação condicional ternária *recomendado*

--- 3_AVANÇANDO/secao-3/src/components/ConditionalRender.jsx
+++ 3_AVANÇANDO/secao-3/src/components/ConditionalRender.jsx
@@ -2,12 +2,27 @@ import { useState } from "react"
 
 function ConditionalRender() {
   const [x] = useState(false);
+
+  const [name, setName] = useState('Zezinho')
   
     return (
     <div>
         <h1>Isso será exibido?</h1>
         {x && <p>Se "x" for true, sim!</p>}
         {!x && <p>Agora x é falso</p>}
+
+        {/* Condição ternária */}
+        <h1>If ternário</h1>
+        {name === "João" ? (
+            <div>
+                <p>O nome é {name}</p>
+            </div>
+        ) : (
+            <div>
+                <p>Nome não encontrando</p>
+            </div>
+        )}
+        <button onClick={() => setName('João')}>Clique   aqui!</button>
     </div>
   )
 }

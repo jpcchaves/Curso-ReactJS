949b9befa1b184e9e2ac246c4d6e8c6df1bf8c52
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-05-29 11:43:16 -0300

    importando imgs REACT

--- 3_AVANÇANDO/secao-3/src/App.js
+++ 3_AVANÇANDO/secao-3/src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import City from '../src/assets/city.jpg'
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World! Desafio Concluído.</h1>
+      <h3>Colocando imagens</h3>
+      {/* Carregando IMGS em src */}
+      <div>
+        <img src="./img1.jpg" alt="paisagem" />
+      </div>
+      {/* Carregando IMGS dentro da pasta ASSETS (recomendado) */}
+        <img src={City} alt="imagem da cidade" />
     </div>
   );
 }

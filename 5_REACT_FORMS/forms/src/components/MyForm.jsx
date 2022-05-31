9c7fce30998514bbb66a24291ba05f8882898e31
João Paulo <95300143+jpcchaves@users.noreply.github.com>
2022-05-31 11:36:05 -0300

    label envolvendo o input (*recomendado*)

--- 5_REACT_FORMS/forms/src/components/MyForm.jsx
+++ 5_REACT_FORMS/forms/src/components/MyForm.jsx
@@ -10,6 +10,12 @@ const MyForm = () => {
           <label htmlFor="name">Nome:</label>
           <input type="text" name="name" placeholder='Digite o seu nome...' />
         </div>
+        {/* 2- Label envolvendo input */}
+          {/* Prática recomendada! */}
+        <label>
+          <span>E-mail</span>
+          <input type="email" name="email" placeholder='Digite o seu e-mail'/>
+        </label>
         <input type="submit" value="enviar" />
       </form>
     </div>

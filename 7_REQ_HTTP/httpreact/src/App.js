04ab0fc106a3fa4b17a06595921d2a6f6abd311c
João Paulo <95300143+jpcchaves@users.noreply.github.com>
2022-06-02 11:49:46 -0300

    axios http request GET added

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useState, useEffect } from 'react';
+
+
+const client = axios.create({
+  baseURL: "http://localhost:3000/products"
+});
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    async function getProducts() {
+      const resp = await client.get("/")
+      setProducts(resp.data)
+    }
+    getProducts()
+  }, [])
+
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <li>{products[0].name}</li>
+      <li>{products[1].name}</li>
+      <li>{products[2].name}</li>
     </div>
   );
 }

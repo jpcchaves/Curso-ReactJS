ffaeec20306f49ec7ae8622a78b0a756ca9dea6b
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-02 22:03:05 -0300

    added the dinamic data insert

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -37,6 +37,14 @@ function App() {
       },
       body: JSON.stringify(product)
     })
+
+    // 3 - Carregamento dinâmico
+    const addedProduct = await res.json()
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct])
+
+    setName("")
+    setPrice("")
   }
 
 

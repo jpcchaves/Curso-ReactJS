cffbc5bd6a2fee54b64e523f43b7592652330c7e
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-02 22:20:48 -0300

    using the custom hook get

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,70 +1,86 @@
-import './App.css';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
+// 4- custom hook
+import { useFetch } from "./hooks/useFetch";
 
-const url = "http://localhost:3000/products"
+const url = "http://localhost:3000/products";
 
 function App() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
-  const [name, setName] = useState("")
-  const [price, setPrice] = useState("")
+  // 4- custom hook
+  const { data: items } = useFetch(url)
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
   // 1- resgatando dados
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(url)
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await fetch(url)
 
-      const data = await res.json()
+  //     const data = await res.json()
 
-      setProducts(data)
-    }
-    fetchData()
-  }, [])
+  //     setProducts(data)
+  //   }
+  //   fetchData()
+  // }, [])
 
   // 2 - add de produtos
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const product = {
       name,
-      price
-    }
+      price,
+    };
 
     const res = await fetch(url, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(product)
-    })
+      body: JSON.stringify(product),
+    });
 
     // 3 - Carregamento dinâmico
-    const addedProduct = await res.json()
+    const addedProduct = await res.json();
 
-    setProducts((prevProducts) => [...prevProducts, addedProduct])
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);
 
-    setName("")
-    setPrice("")
-  }
-
+    setName("");
+    setPrice("");
+  };
 
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
       <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.name} - R$ {product.price}</li>
+        {items && items.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$ {product.price}
+          </li>
         ))}
       </ul>
       <div className="add-product">
         <form onSubmit={handleSubmit}>
           <label>
             Nome:
-            <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </label>
           <label>
             Preço:
-            <input type="number" name="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+            <input
+              type="number"
+              name="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
           </label>
           <input type="submit" value="criar" />
         </form>

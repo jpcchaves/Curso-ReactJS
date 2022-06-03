13ae2b31096fb8832e8ed0f92e8699ac1ee2c0ef
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-02 22:40:40 -0300

    HOOK PERSONALIZADO *IMPORTANTE*

--- 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
+++ 7_REQ_HTTP/httpreact/src/hooks/useFetch.js
@@ -5,6 +5,24 @@ import { useState, useEffect } from "react";
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
 
+  // 5 - refatorando o POST
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(false);
+
+  const httpConfig = (data, method) => {
+    if (method === "POST") {
+      setConfig({
+        method,
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+      setMethod(method)
+    }
+  }
+
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch(url);
@@ -15,6 +33,23 @@ export const useFetch = (url) => {
     };
 
     fetchData();
-  }, [url]);
-  return { data }; 
+  }, [url, callFetch]);
+
+  //  5 - refatorando o post
+  useEffect(() => {
+    const httpRequest = async () => {
+      if (method === "POST") {
+        let fetchOptions = [url, config];
+
+        const res = await fetch(...fetchOptions);
+
+        const json = await res.json();
+
+        setCallFetch(json);
+      }
+    };
+    httpRequest()
+  }, [config, method, url]);
+
+  return { data, httpConfig };
 };

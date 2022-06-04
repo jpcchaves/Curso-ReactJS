08e399fd3958c6d83fe02ed876192ff4f0c895a7
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-04 14:08:28 -0300

    navlink .active

--- 8_ROUTER/react-router/src/components/NavBar.jsx
+++ 8_ROUTER/react-router/src/components/NavBar.jsx
@@ -2,13 +2,16 @@
 import "./NavBar.css";
 
 // Link Component
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="./About">About</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="./About">About</Link> */}
+      {/* Utilizando o NavLink, podemos adicionar a classe de estilo dinamicamente ".active" quando o elemento é selecionado */}
+      <NavLink to="/">Home</NavLink> 
+      <NavLink to="/about">Sobre</NavLink>
     </nav>
   );
 };

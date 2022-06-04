ae4759ec897891e09cc5f3e5214793754ecc5d09
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-04 13:30:29 -0300

    carregando produtos dinamicamente

--- 8_ROUTER/react-router/src/pages/Product.jsx
+++ 8_ROUTER/react-router/src/pages/Product.jsx
@@ -1,13 +1,32 @@
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
 const Product = () => {
-    // 4- rota dinamica
-    // o useParams nos trás um objeto com todos os parâmetros da url
-    const {id } = useParams()
+  // 4- rota dinamica
+  // o useParams nos trás um objeto com todos os parâmetros da url
+  const { id } = useParams();
+
+  // 5- carregamento dinâmico individual
+  const url = "http://localhost:4000/products/" + id;
+
+  const { data: product, loading, error } = useFetch(url);
+
+  console.log(product);
+
   return (
     <>
-    <p>ID do produto: {id}</p>
+      <p>ID do produto: {id}</p>
+      {error && (
+        <p>Ocorreu um erro ao carregar o produto.... Tente novamente</p>
+      )}
+      {loading && <p>Carregando...</p>}
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>R$ {product.price}</p>
+        </div>
+      )}
     </>
-  )
-}
-export default Product
+  );
+};
+export default Product;
